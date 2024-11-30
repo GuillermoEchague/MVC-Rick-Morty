6fe12909858f0f60aa04fcab078335571401c15f
Guillermo Echague <46538216+GuillermoEchague@users.noreply.github.com>
2024-11-30 13:45:45 -0300

    Initial Commit

---
+++ MVC1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC1
+//
+//  Created by Guillermo Echague on 30-11-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MVC1UITests/MVC1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MVC1UITestsLaunchTests.swift
+//  MVC1UITests
+//
+//  Created by Guillermo Echague on 30-11-24.
+//
+
+import XCTest
+
+final class MVC1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
